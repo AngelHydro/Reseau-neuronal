edfaafa67d3c2bceb9f8b9eb9727cd528f3bf9ec
AngelHydro <sanchez.angel.informatique@gmail.com>
2026-07-21 22:53:26 +0200

    0.1.1.1
    Refactor model.py: Enhance docstrings for MLP class and methods

--- src/model.py
+++ src/model.py
@@ -1,18 +1,23 @@
-import torch
+"""Définition d'un perceptron multicouche simple.
+
+Ce module implémente un réseau de neurones feedforward composé
+principalement de couches linéaires suivies d'une activation ReLU.
+"""
+
 import torch.nn as nn
-import yaml
-import pandas as pd
-import sklearn
+
 
 class MLP(nn.Module):
+    """Classe représentant un réseau de neurones multicouche simple."""
+
     def __init__(self, input, hidden, output):
         super(MLP, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(input, hidden),
             nn.ReLU(),
-            nn.Linear(hidden, output)
+            nn.Linear(hidden, output),
         )
-    
+
     def forward(self, x):
-        x = self.model(x)
-        return x
+        """Effectue la propagation avant du modèle."""
+        return self.model(x)
